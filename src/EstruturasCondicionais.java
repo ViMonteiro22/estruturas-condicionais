f0853a166351ac268b5e752f0895ba996bd4abeb
victoriamonteiro <victoria.vrsm@gmail.com>
2026-08-30 22:20:14 -0300

    Estrutura condicional IF e !

---
+++ src/EstruturasCondicionais.java
@@ -0,0 +1,20 @@
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        // if (idade >= 18) Tem essa possibilidade
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Você pode comprar bebida alcoólica!");
+        }
+
+        // ! (NEGAÇÃO)
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Você não pode comprar bebida alcoólica!");
+        }
+    }
+}
