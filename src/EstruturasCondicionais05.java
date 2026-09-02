cf44e95ceb874d7380d99113d98d785bdc0ddb36
victoriamonteiro <victoria.vrsm@gmail.com>
2026-09-02 13:57:25 -0300

    Exercício de SWITCH

--- src/EstruturasCondicionais05.java
+++ src/EstruturasCondicionais05.java
@@ -13,5 +13,29 @@ public class EstruturasCondicionais05 {
                 System.out.println("Inválido");
                 break;
         }
+
+        // Exercício:
+        // Utilizando Switch e dados os valores de 1 a 7
+        // Imprima se é dia útil ou final de semana
+        // Considerando 1 como domingo
+
+        byte dia = 5;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de Semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia Útil");
+                break;
+            default:
+                System.out.println("Dia Inválido");
+                break;
+        }
     }
 }
